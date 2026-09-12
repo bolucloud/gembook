5fe93fd8c2a3cdd4f1381797f17aba5dc831e433
bolucloud <bolucloud>
2026-09-12 18:45:10 -0500

    add list and exit menu options

--- contact_book.rb
+++ contact_book.rb
@@ -14,3 +14,28 @@ end
 def save_contact_book(contact_book_file)
     File.write(FILE, JSON.pretty_generate(contact_book_file))
 end
+
+contact_book = load_contact_book
+
+loop do
+    puts "Gembooks Menu"
+    puts "1. List all contacts"
+    puts "0. Exit"
+
+    print "Make a selection: "
+    selection = gets.chomp.to_i #forcing it to be an integer
+
+    if selection == 1
+        puts "Showing all contacts: "
+        contact_book.each_with_index do |contact, index|
+            puts "#{index + 1}. Name: #{contact['name']}, Email: #{contact['email']}, Phone: #{contact['phone']}"
+        end
+
+    elsif selection == 0
+        puts "Leaving Gembooks. Goodbye!"
+        break
+    
+    else
+        puts "Invalid selection. Please select another option"
+    end
+end
